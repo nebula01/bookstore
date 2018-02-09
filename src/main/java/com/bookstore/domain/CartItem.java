68bd0025915914dc1d03411e7db07fb9ba2b2400
pch <parkchyo@gmail.com>
2018-02-09 17:27:00 +0900

    장바구니기능첫도메인까지

---
+++ src/main/java/com/bookstore/domain/CartItem.java
@@ -0,0 +1,88 @@
+package com.bookstore.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class CartItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private int qty;
+	private BigDecimal subtotal;
+
+	@OneToOne // 한 권의 책은 하나의 아이템박스에 
+	private Book book;
+
+	@OneToMany(mappedBy = "cartItem") // 하나의 아이템 박스는 여러개의 리스트에 
+	@JsonIgnore
+	//private List<BookToCartItem> bookToCartItemList;
+
+	@ManyToOne // 여러개의 아이템박스는 하나의 장바구니에
+	@JoinColumn(name = "shopping_cart_id")
+	private ShoppingCart shoppingCart;
+
+	@ManyToOne // 여러개의 아이템박스는 하나의 주문에
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public BigDecimal getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(BigDecimal subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+}
