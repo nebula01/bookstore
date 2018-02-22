407517070c896f2a96886c04405ebe197ffafe39
pch <parkchyo@gmail.com>
2018-02-22 12:42:09 +0900

    주소관련 스크립트 수정, 구매 정보 수정

--- src/main/java/com/bookstore/controller/CheckoutController.java
+++ src/main/java/com/bookstore/controller/CheckoutController.java
@@ -24,6 +24,7 @@ import com.bookstore.service.BillingAddressService;
 import com.bookstore.service.CartItemService;
 import com.bookstore.service.PaymentService;
 import com.bookstore.service.ShippingAddressService;
+import com.bookstore.service.UserPaymentService;
 import com.bookstore.service.UserService;
 import com.bookstore.service.UserShippingService;
 import com.bookstore.utility.KRConstants;
@@ -52,12 +53,12 @@ public class CheckoutController {
 	@Autowired
 	private BillingAddressService billingAddressService;
 
-	//@Autowired
-	// private UserShippingSerivce userShippingService;
+	@Autowired
+	private UserShippingService userShippingService;
 
-	//@Autowired
-	//private UserPaymentService userPaymentServic;
-	
+	@Autowired
+	private UserPaymentService userPaymentService;
+
 	@RequestMapping("/checkout")
 	public String checkout(@RequestParam("id") Long id,
 			@RequestParam(value = "missingRequiredField", required = false) boolean missingRequiredField, Model model,
@@ -137,21 +138,20 @@ public class CheckoutController {
 		return "checkout";
 	}
 
+	// 자주 쓰는 배송지 주소
 	@RequestMapping("/setShippingAddress")
 	public String setShippingAddress(@RequestParam("userShippingId") Long id, Principal principal, Model model) {
 
-	//	User user = userService.findByUsername(principal.getName());
+		User user = userService.findByUsername(principal.getName());
 
-		//UserShipping userShipping = userShippingService.findById(id);
+		UserShipping userShipping = userShippingService.findById(id);
 
-		/*if (userShipping.getUser().getId() != user.getId()) {
+		if (userShipping.getUser().getId() != user.getId()) {
 			return "badRequestPage";
 		} else {
 			shippingAddressService.setByUserShipping(userShipping, shippingAddress);
 
 			List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
-
-			BillingAddress billingAddress = new BillingAddress();
 
 			model.addAttribute("shippingAddress", shippingAddress);
 			System.out.println(shippingAddress.getShippingAddressName());
@@ -173,26 +173,72 @@ public class CheckoutController {
 			model.addAttribute("shippingAddress", shippingAddress);
 
 			model.addAttribute("classActiveShipping", true);
-			
+
 			if (userPaymentList.size() == 0) {
 				model.addAttribute("emptyPaymentList", true);
 			} else {
 				model.addAttribute("emptyPaymentList", false);
 			}
-			
+
 			model.addAttribute("emptyShippingList", false);
 
 			return "checkout";
 		}
 	}
-*/	
+
+	// 자주 쓰는 카드 등록
 	@RequestMapping("/setPaymentMethod")
-	public String setPaymentMethod(
-			@RequestParam("userPaymentId") Long id, Principal principal,
-			Model model) {
-		
+	public String setPaymentMethod(@RequestParam("userPaymentId") Long id, Principal principal, Model model) {
+
+		// 로그인한 유저 찾음
 		User user = userService.findByUsername(principal.getName());
-		
-		return "";
+
+		// 유저의 구매정보를 찾음
+		UserPayment userPayment = userPaymentService.findById(id);
+
+		// 유저 구매정보에서 청구지 정보를 찾음
+		UserBilling userBilling = userPayment.getUserBilling();
+
+		if (userPayment.getUser().getId() != user.getId()) {
+			return "badRequestPage";
+		} else {
+			paymentService.setByUserPayment(userPayment, payment);
+
+			List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
+
+			billingAddressService.setByUserBilling(userBilling, billingAddress);
+
+			model.addAttribute("shippingAddress", shippingAddress);
+			model.addAttribute("payment", payment);
+			model.addAttribute("billingAddress", billingAddress);
+			model.addAttribute("cartItemList", cartItemList);
+			model.addAttribute("shoppingCart", user.getShoppingCart());
+
+			List<String> stateList = KRConstants.listOfKRStatesCode;
+			Collections.sort(stateList);
+			model.addAttribute("stateList", stateList);
+
+			List<UserShipping> userShippingList = user.getUserShippingList();
+			List<UserPayment> userPaymentList = user.getUserPaymentList();
+
+			model.addAttribute("userShippingList", userShippingList);
+			model.addAttribute("userPaymentList", userPaymentList);
+
+			model.addAttribute("shippingAddress", shippingAddress);
+
+			// 템플릿의 구매 부분 보여주기
+			model.addAttribute("classActivePayment", true);
+			
+			// 구매정보이기 때문에 배송정보가 없을 경우 보여주지 않음 
+			if (userShippingList.size() == 0) {
+				model.addAttribute("emptyShippingList", true);
+			} else {
+				model.addAttribute("emptyShippingList", false);
+			}
+
+			model.addAttribute("emptyPaymentList", false);
+
+			return "checkout";
+		}
 	}
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,6 +2,7 @@
  * 
  */
 
+// 수량 텍스트 박스 수정 시 css바꿈
 $(document).ready(function() {
 	// class이름 cartItemQty
 	$(".cartItemQty").on('change', function() {
@@ -10,4 +11,21 @@ $(document).ready(function() {
 		// update-item의 id의 css를 바꿈
 		$("#update-item-"+id).css('display', 'inline-block');
 	});
+	
+	// #theSameAsShippingAddress에 click이벤트 걸어줌
+	$("#theSameAsShippingAddress").on('click', checkBillingAddress);
 });
+
+// 배송지 주소와 같게 체크시 값 가져오기
+function checkBillingAddress() {
+	if ($("#theSameAsShippingAddress").is(":checked")) {
+		$(".billingAddress").prop("disabled", true);
+		$("#billingName").val($("#shippingName").val());
+		$("#billingAddress").val($("#shippingStreet").val());
+		$("#billingCity").val($("#shippingCity").val());
+		$("#billingState").val($("#shippingState").val());
+		$("#billingZipcode").val($("#shippingZipcode").val());
+	} else {
+		$(".billingAddress").prop("disabled", false);
+	}
+}
